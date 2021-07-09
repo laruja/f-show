23f484620fe2ea5420aa22887af2cc447afad1c2
laruja <58679555+laruja@users.noreply.github.com>
2021-07-09 22:00:09 +0800

    Update server.ts

--- server/configs/server.ts
+++ server/configs/server.ts
@@ -2,7 +2,6 @@ const mongoDBHost =
   process.env.NODE_ENV === "docker"
     ? 'mongodb://database/test'
     : 'mongodb://localhost/testfund';
-// mongodb://localhost:27017/testfund
 const IP = 'localhost';
 export const ADDRESS =
   process.env.NODE_ENV === "docker" ?
